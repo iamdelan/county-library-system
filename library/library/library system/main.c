6a1c012e6deab6c0495ef406c231a48bcffce97c
iamdelan <92990983+iamdelan@users.noreply.github.com>
2022-03-01 14:46:56 +0300

    Add files via upload

---
+++ library/library/library system/main.c
@@ -0,0 +1,25 @@
+/*
+  project: County library management system.
+  Author: Delan Mugure
+  Date: 29,Feb
+  compiler: GNU GCC
+  Language: C99'
+  License: MIT
+ */
+ #include<stdio.h>
+ #include<stdlib.h>
+
+ int main ()
+ {
+     printf("\tCounty Library\n");
+     printf("Welcome Mr. Delan.\n");
+     printf("What would you like to do?\n");
+     printf("1.Add new User\n");
+     printf("2.Add book.\n");
+     printf("3.Borrow book.\n");
+     printf("4.Return Book.\n");
+     printf("5.Books borrowed.\n");
+     printf("6.Delete members.\n");
+     printf("selected action: ");
+     return 0;
+ }
